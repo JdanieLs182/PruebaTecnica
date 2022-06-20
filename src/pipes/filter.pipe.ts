4fcbdc39e8649158d2ac581814f3c6b66ea02273
Daniel <danielleiva567@gmail.com>
2022-06-20 16:38:09 -0400

    Cambios en busqueda

--- src/pipes/filter.pipe.ts
+++ src/pipes/filter.pipe.ts
@@ -6,7 +6,7 @@ import { Pipe, PipeTransform } from "@angular/core";
 export class FilterPipe implements PipeTransform {
   transform(value: Array<any>, arg: string): any {
     if (value && arg) {
-      return value.filter((d) => d.nombre.indexOf(arg) > -1 || d.id == arg);
+      return value.filter((d) => d.nombre.toLowerCase().indexOf(arg.toLowerCase()) > -1 || d.id == arg);
     }
     return value;
   }
